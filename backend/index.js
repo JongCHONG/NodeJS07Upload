d9767e1435a9248e38b76a899e9b4d3590882070
JongCHONG <yonhoa27@hotmail.fr>
2022-01-05 15:15:34 +0100

    2. Modification de notre json

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ const cors = require("cors")
 const authRoutes = require("./routes/auth")
 const adminRoutes = require("./routes/admin")
 
+// import du package multer
+const multer  = require('multer')
+
 app.use(cors({
   origin: "http://localhost:3000",
   credentials: true
@@ -26,6 +29,23 @@ app.use(passport.session())
 
 app.use("/auth", authRoutes)
 app.use("/admin", adminRoutes)
+
+// on rend accessible le dossier public
+app.use(express.static('public'))
+
+// création d'une "mini" config multer
+// cette ligne va créer le dossier /public/uploads s'il n'existe pas
+const upload = multer({ dest: 'public/uploads/' })
+
+// POST sur la route `/file` qui utilise le middleware multer
+// Le middleware va créer le fichier automatiquement et rendre
+// les informations disponible dans `req.file`
+app.post('/file', upload.single('file'), function (req, res, next) {
+  console.log(req.file)
+	// on renomme le fichier créé
+  fs.renameSync(req.file.path, req.file.originalname)
+  res.send("ok")
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
